ab63a279e1026b0ea0c4ec40774bf1d646e698bf
Altay Aydemir <altay.aydemir@gmail.com>
2017-08-05 11:34:32 +0300

    add new methods to api

--- src/api/index.js
+++ src/api/index.js
@@ -4,24 +4,36 @@ class Api {
     this.baseURL = 'http://api.football-data.org/v1'
   }
 
-  fetchPlayers = () => {
-    // DEMO
-    return this.send('teams/66/players')
+  // Teams of a Specific Competition
+  fetchTeams = (competitionID) => this.send(`${this.baseURL}/competitions/${competitionID}/teams`)
+
+  // Players of a Specific Team
+  fetchPlayers = (teamID) => this.send(`${this.baseURL}/teams/${teamID}/players`)
+
+  // All Players from a Competition
+  fetchAllPlayers = (competitionID) => {
+    return this.fetchTeams(competitionID)
+      .then(({ teams }) => {
+        return Promise
+          .all(teams.map(t => this.send(t._links.players.href)))
+          .then(res => res.map((result, index) => result.players.map(player => Object.assign(player, { team: teams[index] }))))
+          .then(res => res.reduce((src, ind) => src.concat(ind), []))
+      })
   }
 
-  send = (url) => new Promise((resolve, reject) => {
-    fetch(`${this.baseURL}/${url}`, {
-      headers: {
-        'X-Auth-Token': this.token,
-      }
-    })
-      .then(res => res.json())
-      .then(res => resolve(res))
-      .catch(err => reject(err))
+  send = (url) => new Promise((resolve, reject) => fetch(url, {
+    headers: {
+      'X-Auth-Token': this.token,
+    }
   })
+    .then(res => res.json())
+    .then(res => resolve(res))
+    .catch(err => reject(err))
+  )
 }
 
 export default new Api({
-  token: '62ab64a9657c4641a574993fcf699e51',
+  // token: '62ab64a9657c4641a574993fcf699e51',
+  token: '6fb02f02fe274a70bced416ee51794d5',
 })
 
--- src/stores/Player.js
+++ src/stores/Player.js
@@ -1,18 +1,31 @@
 import { observable, action, createTransformer } from 'mobx'
 import Api from '../api'
 
+const storedPlayers = JSON.parse(localStorage.getItem('398:players') || '[]')
+
 class PlayerStore {
-  @observable players = []
+  @observable players = storedPlayers
+  @observable resolved = !!storedPlayers.length
 
   getPlayersByPosition = createTransformer(value =>
     this.players.filter(player => player.position === value)
   )
 
-  @action fetchPlayers() {
-    Api
-      .fetchPlayers()
-      .then(res => this.players = res.players)
+  @action setPlayers(players) {
+    localStorage.setItem('398:players', JSON.stringify(players))
+    this.resolved = true
+    return this.players = players
   }
+
+  @action fetchPlayers() {
+    if (!this.resolved) {
+      Api
+        .fetchAllPlayers('398')
+        .then((res) => this.setPlayers(res))
+        .catch(console.log)
+    }
+  }
+
 }
 
 export default new PlayerStore()
